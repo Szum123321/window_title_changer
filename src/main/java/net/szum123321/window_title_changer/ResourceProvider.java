6ba67905a6f1847dc0c59d06fe347e581ef935cc
szymon <szymek.januszek@gmail.com>
2020-08-19 20:39:40 +0200

    Better error reporting of missing icon files

--- src/main/java/net/szum123321/window_title_changer/ResourceProvider.java
+++ src/main/java/net/szum123321/window_title_changer/ResourceProvider.java
@@ -4,6 +4,8 @@ import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class ResourceProvider {
     private final Path mainPath;
@@ -62,41 +64,61 @@ public class ResourceProvider {
     }
 
     private boolean checkIcons() {
-        boolean ok = true;
+        FormatterStringBuilder builder = new FormatterStringBuilder();
 
         if(WindowTitleChanger.config.icon16x16.equals("")) {
-            WindowTitleChanger.logger.error("No 16x16 icon provided!");
-            ok = false;
+            builder.append("No 16x16 icon provided!");
         } else {
-            File f = iconsFolderPath.resolve(WindowTitleChanger.config.icon16x16).toFile();
-
-            if(!isValidIcon(f)) {
-                WindowTitleChanger.logger.error("File %s is not a valid icon!", f);
-                ok = false;
-            }
+            isValidIcon(iconsFolderPath.resolve(WindowTitleChanger.config.icon16x16).toFile(), builder);
         }
 
         if(WindowTitleChanger.config.icon32x32.equals("")) {
-            WindowTitleChanger.logger.error("No 32x32 icon provided!");
-            ok = false;
+            builder.append("No 32x32 icon provided!");
         } else {
-            File f = iconsFolderPath.resolve(WindowTitleChanger.config.icon32x32).toFile();
-
-            if(!isValidIcon(f)) {
-                WindowTitleChanger.logger.error("File %s is not a valid icon!", f);
-                ok = false;
-            }
+            isValidIcon(iconsFolderPath.resolve(WindowTitleChanger.config.icon32x32).toFile(), builder);
         }
 
-        if(!ok) {
+        if(!builder.isEmpty()) {
+            builder.buildStream().forEach(WindowTitleChanger.logger::error);
             WindowTitleChanger.logger.error("Because of the above error, window icon won't be changed!");
             WindowTitleChanger.logger.error("If you're playing on a modpack, please report this error to modpack's creator!");
         }
 
-        return ok;
+        return builder.isEmpty();
     }
 
-    private boolean isValidIcon(File f) {
-        return f.exists() && !f.isDirectory() && f.getName().substring(f.getName().lastIndexOf(".") + 1).equals("png");
+    private void isValidIcon(File f, FormatterStringBuilder builder) {
+        if(!f.exists())
+            builder.append("File %s doesn't exists!", f);
+        else if(f.isDirectory())
+            builder.append("%s is a directory", f);
+        else if(!f.getName().substring(f.getName().lastIndexOf(".") + 1).equals("png"))
+            builder.append("%s is not a png image!", f);
+    }
+
+    private static class FormatterStringBuilder {
+        private final StringBuilder builder;
+
+        public FormatterStringBuilder () {
+            builder = new StringBuilder();
+        }
+
+        public FormatterStringBuilder append (String msg, Object... args) {
+            builder.append(String.format(msg, args));
+            builder.append("\n");
+            return this;
+        }
+
+        public boolean isEmpty() {
+            return builder.length() == 0;
+        }
+
+        public String build() {
+            return builder.toString();
+        }
+
+        public Stream<String> buildStream() {
+            return Arrays.stream(build().split("\n"));
+        }
     }
 }
